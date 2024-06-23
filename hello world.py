cceb9011212f492d928ea4dc42bf98e1b9c31d0b
vulpes-nubes <nubesvulpes@gmx.com>
2024-06-24 00:56:10 +0200

    hello world first commit

---
+++ hello world.py
@@ -0,0 +1,8 @@
+"""
+This entire project is a baseline for me to show my progress on the W3School course on Python
+I am hoping to learn fast and well
+"""
+#temporary code until I recover some lost work
+
+msg ="Hello world, this is Vulpes Nubes"
+print (msg)
