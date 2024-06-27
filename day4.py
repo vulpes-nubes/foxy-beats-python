91a67ca1d6a27f9686c9fad0dc4d19425907dcbb
Vulpes Nubes <nubesvulpes@gmx.com>
2024-06-27 12:43:28 +0200

    Listing time

--- day4.py
+++ day4.py
@@ -9,3 +9,18 @@ myList.remove("another word") #removes first occurence of the item
 print(myList)
 myList.pop(3) #removes specific position in the list
 print(myList)
+sorryList = ["A List to be deleted", 1234, 45643, "potato"]
+del sorryList #deletes the entire list
+sorryList2 = ["Another sorry list", 352345, 1435, True]
+sorryList2.clear()
+print(sorryList2)
+for x in myList: #runs through list items
+    print(x)
+for i in range(len(myList)): #runs through content of list indexes 
+    print(myList[i])
+j = 0
+while j < len(myList):
+    print(myList[j])
+    j = j + 1
+
+[print(x) for x in myList]
